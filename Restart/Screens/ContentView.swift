91fe0e599619c379dc389a7bf11b3df1e508f463
David Edson <davidedson2030@outlook.com>
2023-04-12 09:36:25 +0200

    final commit/push
    commit

---
+++ Restart/Screens/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  Restart
+//
+//  Created by David Edson on 25.11.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    
+    var body: some View {
+        ZStack {
+            if isOnboardingViewActive {
+                OnboardingView()
+            } else {
+                HomeView()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
